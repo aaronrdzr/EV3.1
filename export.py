c9ce53a3d616af2966b984fe748f6a725c8bc091
aaronrdzr <aaron.rodriguezr@uanl.edu.mx>
2024-11-20 00:22:48 -0600

    Delete export.py

--- export.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-datos= pd.read_csv('C:/Users/aaron/Documents/EV3.1', header= None )
-print(datos.head())
-
-datos.to_excel('C:/Users/aaron/Documents/EV3.1', sheet_name= 'page', engine='openpyxl'   )
